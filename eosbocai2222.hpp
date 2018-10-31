7f6c0cd1f9a0e9bd09b909dc1ab222fe89f5701c
loveblockchain <42955625+loveblockchain@users.noreply.github.com>
2018-11-01 00:53:32 +0800

    Update eosbocai2222.hpp

--- eosbocai2222.hpp
+++ eosbocai2222.hpp
@@ -158,6 +158,7 @@ class eosbocai2222 : public contract
     {
         transaction trx;
         trx.actions.emplace_back(std::forward<Args>(args)...);
+        trx.delay_sec = 5;
         trx.send(next_id(), _self, false);
     }
 
